be06b76dbb467615b750d640e995f957e635873f
limouren <601918780@qq.com>
2020-09-24 11:47:23 +0800

    [init] 初始化core模块架构。

---
+++ cloud-integral/src/main/java/com/r7/core/integral/IntegralApplication.java
@@ -0,0 +1,22 @@
+package com.r7.core.integral;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 文件资源服务启动类
+ *
+ * @author zhongpingli
+ */
+@MapperScan("com.r7.core.integral.mapper")
+@SpringBootApplication(scanBasePackages = {"com.r7.core"})
+@EnableDiscoveryClient
+public class IntegralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegralApplication.class, args);
+    }
+
+}
