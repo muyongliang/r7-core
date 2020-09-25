0a79a7b5cdefe9bf20279aaf9cdc7593b5b4a9f8
limouren <601918780@qq.com>
2020-09-25 18:56:40 +0800

    [R7-140] 根据角色ID修改角色信息。

--- cloud-uim/src/main/java/com/r7/core/uim/mapper/UimUserMapper.java
+++ cloud-uim/src/main/java/com/r7/core/uim/mapper/UimUserMapper.java
@@ -3,5 +3,10 @@ package com.r7.core.uim.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.r7.core.uim.model.UimUser;
 
+/**
+ * 用户mapper层
+ *
+ * @author zhongpingli
+ */
 public interface UimUserMapper extends BaseMapper<UimUser> {
 }
