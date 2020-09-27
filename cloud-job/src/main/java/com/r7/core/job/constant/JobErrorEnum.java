5a13231b0884c2eb41048f4b468b4697c5b7ddae
zs <zhousong@yymcd.com>
2020-09-27 18:56:23 +0800

    [R7-141] 规范部分修改

---
+++ cloud-job/src/main/java/com/r7/core/job/constant/JobErrorEnum.java
@@ -0,0 +1,39 @@
+package com.r7.core.job.constant;
+
+import com.r7.core.common.constant.IError;
+
+/**
+ * @author zs
+ */
+public enum JobErrorEnum implements IError {
+    /**
+     * 任务不存在
+     */
+    JOB_IS_NOT_EXISTS("job_is_not_exists", "任务不存在。"),
+    JOB_ID_IS_NULL("job_id_is_null", "任务id不能为空");
+
+    /**
+     * 错误码
+     */
+    private String code;
+
+    /**
+     * 错误信息
+     */
+    private String message;
+
+    JobErrorEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String getCode() {
+        return null;
+    }
+
+    @Override
+    public String getMessage() {
+        return null;
+    }
+}
