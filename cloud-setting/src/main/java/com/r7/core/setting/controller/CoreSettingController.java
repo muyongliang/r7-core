fd23f5f0a8ee59f622fa120adcfe349bf15a53be
liang <muyongliang@yymcd.com>
2020-09-28 15:36:51 +0800

    [R7-135] core-setting 代码规范修改

--- cloud-setting/src/main/java/com/r7/core/setting/controller/CoreSettingController.java
+++ cloud-setting/src/main/java/com/r7/core/setting/controller/CoreSettingController.java
@@ -1,9 +1,9 @@
 package com.r7.core.setting.controller;
 
 import com.r7.core.common.web.ResponseEntity;
-import com.r7.core.setting.dto.CoreSettingDto;
+import com.r7.core.setting.dto.CoreSettingDTO;
 import com.r7.core.setting.service.CoreSettingService;
-import com.r7.core.setting.vo.CoreSettingVo;
+import com.r7.core.setting.vo.CoreSettingVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +27,7 @@ public class CoreSettingController {
     @ApiOperation(
             value = "根据配置id查询配置信息",
             notes = "配置id是必须的",
-            response = CoreSettingVo.class)
+            response = CoreSettingVO.class)
     @GetMapping("/{id}")
     public ResponseEntity findSettingById(@PathVariable("id") Long id) {
         return ResponseEntity.success(coreSettingService.findSettingById(id));
@@ -37,7 +37,7 @@ public class CoreSettingController {
             value = "新增配置信息",
             response = Integer.class)
     @PostMapping
-    public ResponseEntity saveSetting(@RequestBody @Valid CoreSettingDto coreSettingDto) {
+    public ResponseEntity saveSetting(@RequestBody @Valid CoreSettingDTO coreSettingDto) {
 
         return ResponseEntity.success(coreSettingService.saveSetting(coreSettingDto, 0L));
     }
@@ -48,18 +48,18 @@ public class CoreSettingController {
             response = Integer.class)
     @PutMapping("/{id}")
     public ResponseEntity updateSettingById(@PathVariable("id") Long id,
-                                            @RequestBody @Valid CoreSettingDto coreSettingDto) {
+                                            @RequestBody @Valid CoreSettingDTO coreSettingDto) {
         return ResponseEntity.success(coreSettingService.updateSettingById(id, coreSettingDto, 0L));
     }
 
     @ApiOperation(
             value = "分页查询所有配置信息",
             notes = "",
-            response = CoreSettingVo.class)
+            response = CoreSettingVO.class)
     @GetMapping("/page")
     public ResponseEntity pageSetting(@RequestParam(value = "pageSize", defaultValue = "15") Integer pageSize,
                                       @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
-        return ResponseEntity.success(coreSettingService.pageSetting(pageSize, pageNum));
+        return ResponseEntity.success(coreSettingService.pageSettings(pageSize, pageNum));
 
     }
 }
