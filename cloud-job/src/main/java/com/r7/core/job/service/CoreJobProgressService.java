b2ecd989716314febcfb91af239d336f32dacb59
周松 <zhousong@yymcd.com>
2020-10-09 01:41:28 +0000

    Merge remote-tracking branch 'origin/dev_zs' into dev_zs
    # Conflicts:
    #	cloud-job/src/main/java/com/r7/core/job/controller/CoreJobController.java
    #	cloud-uim/src/main/java/com/r7/core/uim/constant/UimErrorEnum.java
    #	cloud-uim/src/main/java/com/r7/core/uim/model/UimResource.java
    #	cloud-uim/src/main/java/com/r7/core/uim/service/impl/UimRoleServiceImpl.java

---
+++ cloud-job/src/main/java/com/r7/core/job/service/CoreJobProgressService.java
@@ -0,0 +1,38 @@
+package com.r7.core.job.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.r7.core.job.dto.CoreJobProgressDto;
+import com.r7.core.job.model.CoreJobProgress;
+import com.r7.core.job.vo.CoreJobProgressVo;
+
+/**
+ * @author zs
+ * @description: 任务进度服务
+ * @date : 2020-09-28
+ */
+public interface CoreJobProgressService extends IService<CoreJobProgress> {
+
+    /**
+     * 新增任务进度
+     * @param coreJobProgressDto 新增任务进度信息
+     * @param userId 操作人id
+     * @return 返回新增结果
+     */
+    CoreJobProgressVo saveJobProgress(CoreJobProgressDto coreJobProgressDto, Long userId);
+
+    /**
+     * 根据任务进度id修改任务进度
+     * @param id 任务id
+     * @param coreJobProgressDto 任务进度修改信息
+     * @param userId 操作人id
+     * @return 返回修改结果
+     */
+    CoreJobProgressVo updateJobById(Long id, CoreJobProgressDto coreJobProgressDto, Long userId);
+
+    /**
+     * 根据用户id查询任务进度
+     * @param userId 用户id
+     * @return 返回查询结果
+     */
+    CoreJobProgressVo getJobProgressByUserId(Long userId);
+}
--- cloud-uim/src/main/java/com/r7/core/uim/dto/UserSingUpDto.java
+++ cloud-uim/src/main/java/com/r7/core/uim/dto/UserSingUpDto.java
@@ -38,6 +38,4 @@ public class UserSingUpDto {
      * IP地址
      */
     private String ip;
-
-
 }
