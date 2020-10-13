f4672dbbfac995bb455afef788cbc775cf4c2d5b
zs <zhousong@yymcd.com>
2020-10-13 17:56:37 +0800

    [R7-176]账号权限冻结基本功能改

--- cloud-uim/src/main/java/com/r7/core/uim/dto/UimChillSaveListDTO.java
+++ cloud-uim/src/main/java/com/r7/core/uim/dto/UimChillSaveListDTO.java
@@ -6,21 +6,28 @@ import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * @author zs
- * @description: 冻结传输对象
+ * @description:
  * @date : 2020-10-13
  */
 @Data
-@ApiModel("修改冻结传输对象")
-public class UimChillUpdateDTO {
+@ApiModel("批量冻结用户权限传输")
+public class UimChillSaveListDTO {
+    /**
+     * 冻结用户
+     */
+    @ApiModelProperty("冻结用户id")
+    @NotNull(message = "用户id不能为空")
+    private Long userId;
     /**
      * 冻结资源;登录/邀请码/提现/平台冻结
      */
     @ApiModelProperty("冻结资源id")
     @NotNull(message = "资源id不能为空")
-    private Long resourceId;
+    private List<Long> resourceIds;
     /**
      * 描述
      */
